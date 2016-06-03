1c4e4a2f48f3e42e63d207bfdeb81769ebcf9b8d
Ian Makgill <ian@spendnetwork.com>
2016-06-03 15:31:06 +0100

    missing commas - gah

--- js/main_pipeline.js
+++ js/main_pipeline.js
@@ -118,7 +118,7 @@ $(document).ready(function() {
           "columnDefs": [
               { "targets": 0, "data": function(d) { return d.supplier; } },
               { "targets": 1, "data": function(d) { return d.category; } },
-              { "targets": 2, "data": function(d) { return d.description; } }
+              { "targets": 2, "data": function(d) { return d.description; } },
               { "targets": 3, "data": function(d) { return formatMoney(d.contract_value); } },
               { "targets": 4, "data": function(d) { return formatDate(d.end_date); } },
           ],
