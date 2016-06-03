2b40e9ca72d6e73c9b54db7eb4e5e4e6f76ac313
Ian Makgill <ian@spendnetwork.com>
2016-06-03 15:20:27 +0100

    adding description to table in js and html

--- js/main_pipeline.js
+++ js/main_pipeline.js
@@ -119,7 +119,8 @@ $(document).ready(function() {
               { "targets": 0, "data": function(d) { return d.supplier; } },
               { "targets": 1, "data": function(d) { return d.category; } },
               { "targets": 2, "data": function(d) { return formatMoney(d.contract_value); } },
-              { "targets": 3, "data": function(d) { return formatDate(d.end_date); } }
+              { "targets": 3, "data": function(d) { return formatDate(d.end_date); } },
+              { "targets": 4, "data": function(d) { return d.description; } },
           ],
           "searching": false,
           "lengthChange": false,
