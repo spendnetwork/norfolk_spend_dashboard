13fe578c51df3c0801f7cc6153ccfafad8db4eec
HelenRM <helen@spendnetwork.com>
2016-07-07 12:48:11 +0100

    Update sql_norfolk_pipeline.sql

--- SQL/sql_norfolk_pipeline.sql
+++ SQL/sql_norfolk_pipeline.sql
@@ -1,12 +1,12 @@
 select
-  supplier_source_string,
+  supplier_source_string as supplier,
+  contract_end as end_date,
   buyer_category_text1 as category,
-  contract_end,
-  value_total,
+  value_total as contract_value,
   contract_name as description
 from
   pipeline.pipeline
 where
   entity_id = 'E2620_NCC_gov' and
-  contract_end >= '2016-06-01'
+  contract_end >= now() - interval '1 month'
 ;
