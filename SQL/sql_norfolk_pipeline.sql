494cf72c7619a0fa39cf2fa9312fe44ed65039d5
HelenRM <helen@spendnetwork.com>
2016-07-08 09:54:09 +0100

    Update sql_norfolk_pipeline.sql

--- SQL/sql_norfolk_pipeline.sql
+++ SQL/sql_norfolk_pipeline.sql
@@ -1,7 +1,7 @@
 select
   supplier_source_string as supplier,
-  contract_end as end_date,
   buyer_category_text1 as category,
+  contract_end as end_date,
   value_total as contract_value,
   contract_name as description
 from
