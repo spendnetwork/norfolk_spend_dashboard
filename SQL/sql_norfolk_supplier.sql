3c5ed5a1b43bc17139d4562183752c235b19e007
sim <sim__k@hotmail.com>
2016-07-15 14:38:36 +0100

    Updated dataclip for 2015/2016 mapping change.

--- SQL/sql_norfolk_supplier.sql
+++ SQL/sql_norfolk_supplier.sql
@@ -1,17 +1,16 @@
 select
 	supplier.supplier_name
 ,	supplier.org_type
-,	sum(tc_slim.amount_calculated)
+,	sum(trans_clean.amount_calculated)
 ,	EXTRACT(YEAR FROM date_calculated) as YYYY
 from
-	tc_slim
-		join supplier on tc_slim.supplier_id = supplier.supplier_id
-where	
+	trans_clean
+		join supplier on coalesce(trans_clean.supplier_id, LPAD(trans_clean.supplier_industry_num::text, 8, '0') || '_com') = supplier.supplier_id
+where
 	date_calculated > '2010-12-01' and
 	supplier.supplier_id not like 'red%' and
-	tc_slim.entity_id = 'E2620_NCC_gov'
+	trans_clean.entity_id = 'E2620_NCC_gov'
 group by
 	supplier.supplier_name
 ,	supplier.org_type
-,	YYYY
-;
+,	YYYY;
